fb9ab7361aaeadcf3054637db7eae7af6aa907f3
Christopher Siems <siemschristopher@outlook.com>
2024-05-17 21:23:31 -0400

    small change

--- utils/utils.py
+++ utils/utils.py
@@ -3,6 +3,7 @@ from time import sleep
 
 def remote_execute(remote_address : str, cmd : str) -> None:
   ssh_process = subprocess.Popen(['sudo', 'ssh', '-o', 'StrictHostKeyChecking=no', remote_address, cmd])
+  sleep(1)
 
   # Wait for the process to finish printing lines
   for line in ssh_process.stdout:
