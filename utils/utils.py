d9381985426479e27f822c79c64d0a4d79b79b3a
Christopher Siems <siemschristopher@outlook.com>
2024-05-17 20:47:16 -0400

    added timeout to remote execute

--- utils/utils.py
+++ utils/utils.py
@@ -1,4 +1,4 @@
 import subprocess
 
 def remote_execute(remote_address : str, cmd : str) -> None:
-  subprocess.Popen(['sudo', 'ssh', '-o', 'StrictHostKeyChecking=no', remote_address, cmd])
+  subprocess.Popen(['sudo', 'ssh', '-o', 'StrictHostKeyChecking=no', '-o', 'ControlPersist=2m', remote_address, cmd])
