70db0e8e17e703df4f06ee261db4feb74cd9584e
Christopher Siems <siemschristopher@outlook.com>
2024-05-17 21:41:00 -0400

    small change

--- utils/utils.py
+++ utils/utils.py
@@ -1,7 +1,5 @@
 import subprocess
 
 def remote_execute(remote_address : str, cmd : str, ssh_timeout : int) -> None:
-  try:
-    subprocess.run(['sudo', 'ssh', '-o', 'StrictHostKeyChecking=no', remote_address, cmd], timeout = ssh_timeout)
-  except subprocess.TimeoutExpired:
-    pass
+  subprocess.run(['sudo', 'ssh', '-o', 'StrictHostKeyChecking=no', remote_address, cmd], timeout = ssh_timeout)
+  
