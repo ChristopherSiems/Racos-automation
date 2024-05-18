b124121af60eb4fd4c594b5ba2ea3cbd67697729
Christopher Siems <siemschristopher@outlook.com>
2024-05-17 20:19:10 -0400

    small fix

--- utils/utils.py
+++ utils/utils.py
@@ -1,4 +1,4 @@
 import subprocess
 
 def remote_execute(remote_address, cmd):
-  return subprocess.run(['sudo', 'ssh', '-o', 'StrictHostKeyChecking=no', node_address, cmd]).stdout
+  return subprocess.run(['sudo', 'ssh', '-o', 'StrictHostKeyChecking=no', remote_address, cmd]).stdout
