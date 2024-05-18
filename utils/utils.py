796f5be4a6c12dead8b89464ad8e9aea62d86600
Christopher Siems <siemschristopher@outlook.com>
2024-05-17 21:05:29 -0400

    revert

--- utils/utils.py
+++ utils/utils.py
@@ -1,12 +1,4 @@
 import subprocess
 
 def remote_execute(remote_address : str, cmd : str) -> None:
-  ssh_process = subprocess.Popen(['sudo', 'ssh', '-o', 'StrictHostKeyChecking=no', remote_address, cmd], stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
-
-  # Wait for stdout to produce a blank line
-  while True:
-    if not ssh_process.stdout.readline().decode('utf-8'):
-      break
-  
-  ssh_process.stdout.close()
-  ssh_process.wait()
+  ssh_process = subprocess.Popen(['sudo', 'ssh', '-o', 'StrictHostKeyChecking=no', remote_address, cmd])
