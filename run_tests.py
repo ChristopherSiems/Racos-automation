740e2cf444276f103852a4a380418103360f5688
Christopher Siems <siemschristopher@outlook.com>
2024-05-18 22:30:41 -0400

    added sync and async function

--- run_tests.py
+++ run_tests.py
@@ -4,6 +4,7 @@ import subprocess
 from time import sleep
 
 RAFT : str = 'raft 2'
+SSH_ARGS : typing.List[str] = ['sudo', 'ssh', '-o', 'StrictHostKeyChecking=no']
 KILL_ETCD : str = 'killall etcd'
 PROFILE_CONFIGS : typing.Dict[str, str]= {
   "rabia 2": "#!/usr/bin/env bash\n/local/go-ycsb/bin/go-ycsb load etcd -p etcd.endpoints=\"10.10.1.1:2379,10.10.1.2:2379,10.10.1.2.2379,10.10.1.3:2379,10.10.1.4:2379,10.10.1.5:2379\" -P /local/go-ycsb/workloads/workload\n/local/go-ycsb/bin/go-ycsb run etcd -p etcd.endpoints=\"10.10.1.1:2379,10.10.1.2:2379,10.10.1.2.2379,10.10.1.3:2379,10.10.1.4:2379,10.10.1.5:2379\" -P /local/go-ycsb/workloads/workload",
@@ -11,18 +12,19 @@ PROFILE_CONFIGS : typing.Dict[str, str]= {
   "paxos 2": "#!/usr/bin/env bash\n/local/go-ycsb/bin/go-ycsb load etcd -p etcd.endpoints=\"10.10.1.1:2379\" -P /local/go-ycsb/workloads/workload\n/local/go-ycsb/bin/go-ycsb run etcd -p etcd.endpoints=\"10.10.1.1:2379\" -P /local/go-ycsb/workloads/workload"
 }
 
-def remote_execute(remote_address : str, cmd : str, disconnect_timeout : int = 1, return_out : bool = False) -> typing.Union[None, str]:
-  ssh_process = subprocess.Popen(['sudo', 'ssh', '-o', 'StrictHostKeyChecking=no', remote_address, cmd], stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
+def remote_execute_async(remote_address : str, remote_cmd : str, disconnect_timeout : int = 1) -> None:
+  subprocess.Popen(SSH_ARGS + [remote_address, remote_cmd], stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
   sleep(disconnect_timeout)
-  if return_out:
-    return ssh_process.stdout.read().decode('utf-8')
   ssh_process.stdout.close()
+
+def remote_execute_sync(remote_address : str, remote_cmd : str) -> str:
+  return subprocess.run(SSH_ARGS + [remote_address, remote_cmd], stdout = subprocess.PIPE, stderr =  subprocess.STDOUT, universal_newlines = True).stdout
 
 def kill_nodes() -> None:
   print('= killing running ETCD processes =')
   for node_address in nodes_exclusive:
     print('== ' + node_address + ' ==')
-    remote_execute(node_address, KILL_ETCD)
+    remote_execute_async(node_address, KILL_ETCD)
     print('$ ' + KILL_ETCD)
   print('all processes killed')
 
@@ -44,7 +46,7 @@ for alg in [RAFT, 'rabia 2', 'paxos 2']:
   for node_address in nodes_exclusive:
     print('== ' + node_address + ' ==')
     run_cmd : str = 'sh /local/run.sh ' + alg
-    remote_execute(node_address, run_cmd, 30)
+    remote_execute_async(node_address, run_cmd, 30)
     print('$ ' + run_cmd)
   
   client_address : str = node_addresses[-1]
@@ -53,7 +55,7 @@ for alg in [RAFT, 'rabia 2', 'paxos 2']:
   # Determining the Raft leader
   raft_leader_endpoint : str = None
   if alg == RAFT:
-    for node_data in json.loads(remote_execute(client_address, '/local/etcd/ETCD/bin/etcdctl --endpoints=10.10.1.1:2379,10.10.1.2:2379,10.10.1.3:2379,10.10.1.4:2379,10.10.1.5:2379 endpoint status --write-out=json', disconnect_timeout= 15, return_out = True)):
+    for node_data in json.loads(remote_execute_sync(client_address, '/local/etcd/ETCD/bin/etcdctl --endpoints=10.10.1.1:2379,10.10.1.2:2379,10.10.1.3:2379,10.10.1.4:2379,10.10.1.5:2379 endpoint status --write-out=json')):
       node_status : typing.Dict = node_data['Status']
       if node_status['header']['member_id'] == node_status['leader']:
         raft_leader_endpoint = node_data['Endpoint']
@@ -61,8 +63,7 @@ for alg in [RAFT, 'rabia 2', 'paxos 2']:
   profile_string : str = PROFILE_CONFIGS[alg].replace('XXXX', raft_leader_endpoint) if alg == RAFT else PROFILE_CONFIGS[alg]
 
   setup_cmd : str = 'echo ' + profile_string + ' > /local/go-ycsb/workloads/profile.sh'
-  remote_execute(client_address, setup_cmd)
+  remote_execute_async(client_address, setup_cmd)
   print('$ ' + setup_cmd)
-  profile_cmd : str = 'cat /local/go-ycsb/workloads/profile.sh'
-  print(subprocess.run(['sudo', 'ssh', '-o', 'StrictHostKeyChecking=no', client_address, profile_cmd], stdout = subprocess.PIPE, universal_newlines = True).stdout)
+  print(remote_execute_sync(client_address, 'cat /local/go-ycsb/workloads/profile.sh'))
 kill_nodes()
