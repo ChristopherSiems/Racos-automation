7710867c5ec266acf3cfff9df4481d3b18b3a3c8
Christopher Siems <siemschristopher@outlook.com>
2024-05-23 15:03:19 -0400

    added more outputs

--- optimize_operation_count.py
+++ optimize_operation_count.py
@@ -23,18 +23,21 @@ for alg in ALG_TO_NAME:
   setup_alg(nodes_addresses, alg)
   for test_data in all_tests:
     for variable in test_data[1]['variable']:
-      operation_count : int = 1000
+      print('== ' + variable + ' ==')
+      operation_count : int = 2000
       best_error : float = float('inf')
       while True:
-        print('testing ' + str(operation_count) + ' counts')
+        print('testing ' + str(operation_count) + ' operations')
         remote_execute_async(client_address, 'echo "' + test_data[1]['workload'].format(variable = str(variable), operation_count = str(operation_count)) + '" > /local/go-ycsb/workloads/workload')
         curr_error : float = abs(30 - float(FLOAT_PATTERN.findall(RUNTIME_PATTERN.findall(remote_execute_sync(client_address, 'sh /local/go-ycsb/workloads/profile.sh'))[-1])[0]))
-        print('error amount ' + str(curr_error))
+        print('error amount: ' + str(curr_error))
         if curr_error > best_error:
           break
         best_error = curr_error
         operation_count += 1000
-      test_data[1][ALG_TO_NAME[alg] + '-operation_count'].append(operation_count - 1000)
-  for test_data in all_tests:
-    with open('tests/' + test_data[0] + '.json', 'w', encoding = 'utf-8') as config_file:
-      json.dump(test_data[1], config_file, indent = 2)
+      curr_operations : typing.List[int] = test_data[1][ALG_TO_NAME[alg] + '-operation_count']
+      curr_operations.append(operation_count - 1000)
+      print('operation counts: ' + curr_operations)
+for test_data in all_tests:
+  with open('tests/' + test_data[0] + '.json', 'w', encoding = 'utf-8') as config_file:
+    json.dump(test_data[1], config_file, indent = 2)
