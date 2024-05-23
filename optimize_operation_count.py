ef05388deb5a28123a41eda2a44b12d9f371f560
Christopher Siems <siemschristopher@outlook.com>
2024-05-23 14:30:23 -0400

    working!

--- optimize_operation_count.py
+++ optimize_operation_count.py
@@ -29,9 +29,7 @@ for alg in ALG_TO_NAME:
       best_error : float = float('inf')
       while True:
         print('testing ' + str(operation_count) + ' counts')
-        temp = 'echo "' + test_data[1]['workload'].format(variable = str(variable), operation_count = str(operation_count)) + '" > /local/go-ycsb/workloads/workload'
-        print(temp)
-        remote_execute_async(client_address, temp)
+        remote_execute_async(client_address, 'echo "' + test_data[1]['workload'].format(variable = str(variable), operation_count = str(operation_count)) + '" > /local/go-ycsb/workloads/workload')
         curr_error : float = abs(30 - float(FLOAT_PATTERN.findall(RUNTIME_PATTERN.findall(remote_execute_sync(client_address, 'sh /local/go-ycsb/workloads/profile.sh'))[-1])[0]))
         print('error amount ' + str(curr_error))
         if curr_error > best_error:
