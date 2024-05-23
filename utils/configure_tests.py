3539e112ed05d23c3cd091dfc017ae8e6c2b57e4
Christopher Siems <siemschristopher@outlook.com>
2024-05-23 13:52:02 -0400

    fixed incorrect path

--- utils/configure_tests.py
+++ utils/configure_tests.py
@@ -12,7 +12,7 @@ def configure_tests() -> typing.Tuple[typing.Union[typing.List[str], typing.List
       nodes_addresses.append('root@node-' + str(i) + '.' + auto_config_data['experiment_name'] + '.' + auto_config_data['domain'])
     test_info : typing.List[typing.Tuple[typing.Union[str, typing.Dict[str, typing.Union[typing.List[float], typing.List[int], str]]]]] = []
     for curr_test in auto_config_data['tests']:
-      with open(curr_test + '.json', READ, encoding = ENCODING) as test_config:
+      with open('tests/' + curr_test + '.json', READ, encoding = ENCODING) as test_config:
         test_config_data : typing.Dict[str, typing.Union[typing.List[float], typing.List[int], str]] = json.load(test_config)
         test_info.append((curr_test, test_config_data))
     return nodes_addresses, test_info
