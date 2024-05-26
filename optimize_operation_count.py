06f56c6d29c80000dddc7ca5f4ca4277093a9c22
Christopher Siems <siemschristopher@outlook.com>
2024-05-26 15:33:03 -0400

    bdebugging

--- optimize_operation_count.py
+++ optimize_operation_count.py
@@ -32,7 +32,17 @@ for alg in ALG_TO_NAME:
       while True:
         print('testing ' + str(operation_count) + ' operations')
         remote_execute_async(client_address, 'echo "' + test_data[1]['workload'].format(variable = str(variable), operation_count = str(operation_count)) + '" > /local/go-ycsb/workloads/workload')
-        curr_error : float = round(abs(30 - float(FLOAT_PATTERN.findall(RUNTIME_PATTERN.findall(remote_execute_sync(client_address, 'sh /local/go-ycsb/workloads/profile.sh'))[-1])[0])), 2)
+        temp = remote_execute_sync(client_address, 'sh /local/go-ycsb/workloads/profile.sh')
+        print(temp)
+        temp2 = RUNTIME_PATTERN.findall(temp)
+        print(temp2)
+        temp3 = temp2[-1]
+        print(temp3)
+        temp4 = FLOAT_PATTERN.findall(temp3)
+        print(temp4)
+        temp5 = temp4[0]
+        print(temp5)
+        curr_error : float = round(abs(30 - float(temp5)), 2)
         print('error amount: ' + str(curr_error))
         if curr_error > best_error:
           break
