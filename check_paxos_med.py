e0074b4b3db05fb95c84fe93f553ba9d3f2a0626
Christopher Siems <siemschristopher@outlook.com>
2024-06-06 02:09:53 -0400

    fix!

--- check_paxos_med.py
+++ check_paxos_med.py
@@ -14,4 +14,4 @@ client_address : str = node_addresses[-1]
 
 setup_alg(node_addresses, 'paxos 2', num_nodes)
 remote_execute_async(client_address, 'echo "fieldlength=1500000\nrecordcount=500\noperationcount=500\nfieldcount=1\nreadproportion=0.0\nupdateproportion=1.0\nreadmodifywriteproportion=0.0\nscanproportion=0\ninsertproportion=0\nworkload=core\nreadallfields=true\nthreadcount=50\nrequestdistribution=zipfian\nmeasurementtype=raw" > /local/go-ycsb/workloads/workload')
-print(median([int(re.findall(r'd+', output_string)[-1]) for output_string in re.findall(r'UPDATE,\d+,\d+', remote_execute_sync(client_address, 'sh /local/go-ycsb/workloads/profile.sh'))]))
+print(median([int(re.findall(r'\d+', output_string)[-1]) for output_string in re.findall(r'UPDATE,\d+,\d+', remote_execute_sync(client_address, 'sh /local/go-ycsb/workloads/profile.sh'))]))
