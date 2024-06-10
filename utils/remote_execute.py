419b593e2d28666381f30881584a9d932aed8c7a
Christopher Siems <siemschristopher@outlook.com>
2024-06-10 17:58:28 -0400

    small revert

--- utils/remote_execute.py
+++ utils/remote_execute.py
@@ -9,5 +9,5 @@ def remote_execute_async(remote_address : str, remote_cmd : str, disconnect_time
   sleep(disconnect_timeout)
   ssh_process.stdout.close()
 
-def remote_execute_sync(remote_address : str, remote_cmd : str) -> str:
+def remote_execute_sync(remote_address : str, remote_cmd : str) -> typing.IO[str]:
   return subprocess.run(SSH_ARGS + [remote_address, remote_cmd], stdout = subprocess.PIPE, stderr =  subprocess.STDOUT, universal_newlines = True, check = True).stdout
