310b818d5ccd6c43a13c44a27c3e41be1cb874c5
Christopher Siems <siemschristopher@outlook.com>
2024-06-10 11:15:35 -0400

    small change

--- utils/configure_tests.py
+++ utils/configure_tests.py
@@ -10,7 +10,7 @@ def configure_tests() -> typing.Tuple[typing.Union[int, typing.List[str], typing
     nodes_addresses : typing.List[str] = []
     node_count : int = auto_config_data['node_count']
     for i in range(1, node_count + 1):
-      nodes_addresses.append(f'root@node-{i}.{auto_config_data['experiment_name']}.{auto_config_data['domain']}')
+      nodes_addresses.append(f'root@node-{i}.{auto_config_data["experiment_name"]}.{auto_config_data["domain"]}')
     test_info : typing.List[typing.Tuple[typing.Union[str, typing.Dict[str,typing.Union[int, typing.List[float], typing.List[int], str]]]]] = []
     for curr_test in auto_config_data['tests']:
       with open(f'tests/{curr_test}.json', READ, encoding = ENCODING) as test_config:
