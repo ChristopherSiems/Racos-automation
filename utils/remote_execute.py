41cf7573907996ee59fb7ee335217e86b808950c
Christopher Siems <siemschristopher@outlook.com>
2024-06-10 18:48:21 -0400

    trying something new

--- utils/remote_execute.py
+++ utils/remote_execute.py
@@ -10,7 +10,12 @@ def remote_execute_async(remote_address : str, remote_cmd : str, disconnect_time
   ssh_process.stdout.close()
 
 def remote_execute_sync(remote_address : str, remote_cmd : str) -> str:
-  return subprocess.run(SSH_ARGS + [remote_address, remote_cmd], stdout = subprocess.PIPE, stderr =  subprocess.STDOUT, universal_newlines = True, check = True).stdout.decode('utf-8')
+  ssh_process : subprocess.Popen = subprocess.Popen(SSH_ARGS + [remote_address, remote_cmd], stdout = subprocess.PIPE, stderr =  subprocess.STDOUT, universal_newlines = True)
+  try:
+    return ssh_process.communicate(timeout = 60)[0].decode('utf-8')
+  except subprocess.TimeoutExpired:
+    ssh_process.terminate()
+    return ssh_process.communicate()[0].decode('utf-8')
 
 def debug_execute(remote_address : str, remote_cmd) -> str:
   ssh_process = subprocess.Popen(SSH_ARGS + [remote_address, remote_cmd], stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
