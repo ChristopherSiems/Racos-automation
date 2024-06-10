06648164a53d182626d92a313cb1af9d0a17df7f
Christopher Siems <siemschristopher@outlook.com>
2024-06-10 18:26:10 -0400

    added some debug code

--- utils/remote_execute.py
+++ utils/remote_execute.py
@@ -9,5 +9,12 @@ def remote_execute_async(remote_address : str, remote_cmd : str, disconnect_time
   sleep(disconnect_timeout)
   ssh_process.stdout.close()
 
-def remote_execute_sync(remote_address : str, remote_cmd : str) -> typing.IO[str]:
+def remote_execute_sync(remote_address : str, remote_cmd : str) -> str:
   return subprocess.run(SSH_ARGS + [remote_address, remote_cmd], stdout = subprocess.PIPE, stderr =  subprocess.STDOUT, universal_newlines = True, check = True).stdout
+
+def debug_execute(remote_address : str, remote_cmd) -> str:
+  ssh_process = subprocess.Popen(SSH_ARGS + [remote_address, remote_cmd], stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
+  sleep(60)
+  partial_output : str = ssh_process.stdout.read()
+  ssh_process.stdout.close()
+  return partial_output
