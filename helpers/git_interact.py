5761696eb1d211ef849e675446cdd9599e3da023
Christopher Siems <siemschristopher@outlook.com>
2024-06-12 13:40:22 -0400

    small changes

--- helpers/git_interact.py
+++ helpers/git_interact.py
@@ -2,7 +2,7 @@ import typing
 import subprocess
 
 def git_interact(cmd : typing.List[str]) -> None:
-  subprocess.run(['git'] + cmd, stdout = subprocess.PIPE, check = False)
+  subprocess.run(['git'] + cmd, stdout = subprocess.PIPE, stderr = subprocess.PIPE, check = False)
 
 def git_add(cmd : str) -> None:
   git_interact(['add'] + [cmd])
