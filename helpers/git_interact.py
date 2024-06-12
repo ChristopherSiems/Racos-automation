fc5d4c84853847ccfab24bec45cced260b870b66
Christopher Siems <siemschristopher@outlook.com>
2024-06-12 11:12:14 -0400

    small changes to git interact

--- helpers/git_interact.py
+++ helpers/git_interact.py
@@ -1,8 +1,12 @@
 import typing
 import subprocess
 
+from helpers.custom_prints import four_equal_print
+
 def git_interact(cmd : typing.List[str]) -> None:
+  four_equal_print()
   subprocess.run(['sudo', 'git'] + cmd, check = False)
+  four_equal_print()
 
 def git_add(cmd : str) -> None:
   git_interact(['add'] + [cmd])
