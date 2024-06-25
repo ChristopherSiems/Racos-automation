36d9e7552c1d7c93dd07fa3ee8598e26622b5b06
Christopher Siems <siemschristopher@outlook.com>
2024-06-24 20:07:56 -0400

    fixing something small

--- reset_nodes.py
+++ reset_nodes.py
@@ -2,10 +2,10 @@
 
 import typing
 
-from helpers.reset_nodes import reset_nodes, remove_delay
+from helpers.reset_nodes import reset_nodes, remove_delay_packet_drop
 from helpers.configure_tests import configure_tests
 
 node_addresses : typing.List[str] = configure_tests()[1]
 
 reset_nodes(node_addresses[:-1])
-remove_delay(node_addresses)
+remove_delay_packet_drop(node_addresses)
