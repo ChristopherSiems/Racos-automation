219a8311fad90e59f8f819fc7509152f5eb8ee12
Christopher Siems <siemschristopher@outlook.com>
2024-07-10 17:18:17 -0400

    pls work

--- reset_nodes.py
+++ reset_nodes.py
@@ -4,7 +4,7 @@ import typing
 
 from helpers.reset_nodes import reset_delay_packets_cpus, reset_nodes
 
-total_nodes : int = argv[1]
+total_nodes : int = int(argv[1])
 
 reset_nodes(total_nodes)
 reset_delay_packets_cpus(total_nodes)
