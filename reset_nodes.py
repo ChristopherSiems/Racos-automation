5f6cd6e95aeb945e1021ddaa0f23d8dd9af94937
Christopher Siems <siemschristopher@outlook.com>
2024-07-10 17:17:23 -0400

    another small fix

--- reset_nodes.py
+++ reset_nodes.py
@@ -2,10 +2,9 @@
 from sys import argv
 import typing
 
-from helpers.encoding import configure_tests, ip_lister
 from helpers.reset_nodes import reset_delay_packets_cpus, reset_nodes
 
-node_ips : typing.List[str] = ip_lister(argv[1])
+total_nodes : int = argv[1]
 
-reset_nodes(node_ips[:-1])
-reset_delay_packets_cpus(node_ips)
+reset_nodes(total_nodes)
+reset_delay_packets_cpus(total_nodes)
