f5289a303b4dfe126ddf268061fadc6def7c9136
Christopher Siems <siemschristopher@outlook.com>
2024-07-11 14:11:38 -0400

    fixing small issue

--- helpers/encoding.py
+++ helpers/encoding.py
@@ -20,6 +20,14 @@ def config_matches(pattern : re.Pattern, config : str) -> bool:
   '''
   return bool(re.match(pattern, config))
 
+def config_to_str(config : typing.List[float]) -> str:
+  '''
+  encodes delay and packet loss configs as strings
+  :param config: the list of values making up the config
+  :returns: the config in string representation
+  '''
+  return "_".join(list(map(str, config)))
+
 def ip_lister(node_count : int) -> typing.List[str]:
   '''
   builds a list of ips from a number of nodes
--- legacy/legacy_encoding.py
+++ legacy/legacy_encoding.py
@@ -52,14 +52,6 @@ def config_to_offset(config : str, style : str) -> float:
     if re.match(POINT_FIVE_CONFIG_PATTERN, config): return 0
     if re.match(ONE_CONFIG_PATTERN, config): return .32
 
-def config_to_str(config : typing.List[float]) -> str:
-  '''
-  encodes delay and packet loss configs as strings
-  :param config: the list of values making up the config
-  :returns: the config in string representation
-  '''
-  return "_".join(list(map(str, config)))
-
 ZERO_CONFIG_PATTERN : re.Pattern = re.compile(r'^0(_0)*$')
 POINT_ONE_CONFIG_PATTERN : re.Pattern = re.compile(r'^0\.1(_0\.1)*$')
 POINT_FIVE_CONFIG_PATTERN : re.Pattern = re.compile(r'^0\.5(_0\.5)*$')
