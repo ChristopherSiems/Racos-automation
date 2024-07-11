283570dc93313b9e92fbdac5f0a4c9f2d6505f2f
Christopher Siems <siemschristopher@outlook.com>
2024-07-11 17:37:28 -0400

    q

--- run_tests.py
+++ run_tests.py
@@ -12,8 +12,8 @@ from helpers.execute import git_interact, remote_execute_async, remote_execute_s
 from helpers.reset_nodes import reset_delay_packets_cpus, reset_nodes
 
 ALG_COUNTS : typing.Dict[str, str] = {
-  'racos' : '2000',
-  'tracos' : '2000',
+  'racos' : '1000',
+  'tracos' : '1000',
   'rabia' : '1900',
   'raft' : '900',
   'paxos' : '1900'
