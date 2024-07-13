adb7b44c2cde7c7e76c3169456e6c40b81165702
Christopher Siems <siemschristopher@outlook.com>
2024-07-12 22:59:08 -0400

    small fix

--- run_tests.py
+++ run_tests.py
@@ -138,7 +138,7 @@ CPU freq: {cpu_freqs}''')
 
       # configures `profile.sh` and `workload` for the current algorithm
       if alg == 'paxos': profile_string = PROFILE_CONFIG.format(leader_endpoint = '10.10.1.1:2379')
-      else: profile_string = PROFILE_CONFIG.format(leader_endpoint = ','.join([f"{node_ip}:2379" if node_ip != "10.10.1.2" else f"{node_ip}:2379,{node_ip}:2379" for node_ip in node_ips_list]))
+      else: profile_string = PROFILE_CONFIG.format(leader_endpoint = ','.join([f"{node_ip}:2379" if node_ip != "10.10.1.2" else f"{node_ip}:2379,{node_ip}:2379" for node_ip in node_ips_list[:-1]]))
       workload_cmd : str = SCRIPT_LOADER.format(script = test_config["workload"].format(variable = str(variable), counts = COUNTS[i] if test.startswith('data_size') else COUNTS[7 - i]) if not test.startswith('scalability') else test_config['workload'], path = '/local/go-ycsb/workloads/workload')
       bash_print(workload_cmd)
       remote_execute_async(client_address, workload_cmd)
